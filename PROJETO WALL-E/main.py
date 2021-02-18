b9cb614c0f3b1ea89edc3d7e266e945493cfaceb
Paolo Prodossimo Lopes <70642432+PaoloProdossimoLopes@users.noreply.github.com>
2021-02-18 08:13:28 -0300

    update main with mic

--- PROJETO WALL-E/main.py
+++ PROJETO WALL-E/main.py
@@ -1,1 +1,11 @@
 #main code
+import speech_recognition as sr
+
+#create a recognazer
+r = sr.Recognizer()
+
+#open mic to recorder
+with sr.Microphone() as source:
+    audio = r.listen(source) #define a fonte de audio como microfone
+
+    print(r.recognize_google())
