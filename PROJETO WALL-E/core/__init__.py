ffa863dfc8bc3ea8e61ef59abd9e39efdaa8fb2e
Paolo Prodossimo Lopes <70642432+PaoloProdossimoLopes@users.noreply.github.com>
2021-02-19 17:44:51 -0300

    Add somes comand voices

--- PROJETO WALL-E/core/__init__.py
+++ PROJETO WALL-E/core/__init__.py
@@ -12,3 +12,21 @@ class SystemInfo:
         answer = f'São {now.hour} horas e {now.minute} Minutos'
         return answer
         #print(now.year, now.month, now.day, now.hour,now.minute, now.second)
+
+    def get_day():
+        now = datetime.datetime.now()
+        answer = f'Hoje é dia {now.day} '
+        return answer
+        #print(now.year, now.month, now.day, now.hour,now.minute, now.second)
+    
+    def eu_sou():
+        answer = "eu sou Uuuuaaallyyyye"
+        return answer
+
+    def blessed():
+        answer = 'amem'
+        return answer
+    
+    def criador():
+        answer = 'quen me criou foi o paolo'
+        return answer
--- PROJETO WALL-E/main.py
+++ PROJETO WALL-E/main.py
@@ -51,5 +51,22 @@ while True:
             print(text)
             #fala(text)#repete oq fala
 
+            # Comando easter egg:
+            if text == "quem criou" or text == "criador" or text == "criado" or  text == "criada":
+                fala(core.SystemInfo.criador())
+
+            # Comando easter egg:
+            if text == "qual é seu nome" or text == "seu nome" or text == "como você se chama" or  text == "quem é voce":
+                fala(core.SystemInfo.eu_sou())
+
+            # Comando Hora
             if text == "que horas são" or text == "me diga as horas" or text == "fala a hora":
                 fala(core.SystemInfo.get_time())
+
+            # Comando amem
+            if text == "oração":
+                fala(core.SystemInfo.blessed())
+
+            # Comando dia
+            if text == "que dia é hoje" or text == "me diga o dia" or text == "hoje é":
+                fala(core.SystemInfo.get_day())
